b113a90af8075e7ae2ee1f11cc07fbde3502a505
jmtblei <47536941+jmtblei@users.noreply.github.com>
2019-05-10 09:28:03 -0700

    added register/login functions

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,13 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const userdb = require('../users/users-model');
 
 const { authenticate } = require('../auth/authenticate');
+const jwtKey =
+  process.env.JWT_SECRET ||
+  'add a .env file to root of project with the JWT_SECRET variable';
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +17,50 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  userdb.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  userdb.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token,
+        });
+      } else {
+        res.status(401).json({ message: 'invalid credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+  };
+  const options = {
+    expiresIn: '24h',
+  };
+
+  return jwt.sign(payload, jwtKey, options);
 }
 
 function getJokes(req, res) {
